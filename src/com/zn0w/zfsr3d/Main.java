aa897b267e47ef3a97db8940428229e41efb9d58
Daniil Blagiy <blagiy.daniil@gmail.com>
2020-03-18 18:42:02 +0300

    working on recursive file tree extraction

--- src/com/zn0w/zfsr3d/Main.java
+++ src/com/zn0w/zfsr3d/Main.java
@@ -13,26 +13,51 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World! It's Zn0w File System Respresentation 3D");
 		
-		String dirName = "D:\\work";
+		String dirName = "C:\\Users\\blagi\\Desktop\\test";
 		
-		FileTree fs = new FileTree(new Node(dirName));
-
         try {
-			Files.list(new File(dirName).toPath())
-			        //.limit(10)
-			        .forEach(path -> {
-			            System.out.println(path);
-			            fs.getRoot().addChild(new Node(fs.getRoot(), path.getFileName().toString()));
-			        });
-			
-			ArrayList<Node> components = fs.getRoot().getChildren();
-			for (int i = 0; i < components.size(); i++) {
-				System.out.println("Node " + i + ": " + components.get(i).getComponentName());
-			}
+        	@SuppressWarnings("unused")
+			FileTree fs = getFSTree(dirName);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	// TODO move methods below in a separate class
+	
+	static public FileTree getFSTree(String root_dir) throws IOException {
+		FileTree fs = new FileTree(new Node(root_dir));
+		
+		Files.list(new File(root_dir).toPath())
+        .forEach(path -> {
+            Node node = new Node(fs.getRoot(), path.getFileName().toString());
+            try {
+				getChildren(node, root_dir + "/");
+				fs.getRoot().addChild(node);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+        });
+		
+		return fs;
+	}
+	
+	static public void getChildren(Node node, String abs_path) throws IOException {
+		// if it's not a directory
+		if (node.getComponentName().contains(".")) {
+			return;
+		}
+		
+		Files.list(new File(abs_path + node.getComponentName()).toPath())
+        .forEach(path -> {
+            Node child_node = new Node(node, path.getFileName().toString());
+        	try {
+				getChildren(child_node, abs_path + node.getComponentName() + "/");
+				node.addChild(child_node);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+        });
 	}
 	
 }
