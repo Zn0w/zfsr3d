e2ff5a250b8565203878c1a6343881a8e3c012ef
Daniil Blagiy <blagiy.daniil@gmail.com>
2020-03-18 17:20:19 +0300

    initial commit

---
+++ src/com/zn0w/zfsr3d/Main.java
@@ -0,0 +1,9 @@
+package com.zn0w.zfsr3d;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World! It's Zn0w File System Respresentation 3D");
+	}
+	
+}
