74382e59e53044828cb3509af439f271fbf6f6d2
Daniil Blagiy <blagiy.daniil@gmail.com>
2020-03-19 15:15:33 +0300

    moved FileTree object generation to special class

--- src/com/zn0w/zfsr3d/Main.java
+++ src/com/zn0w/zfsr3d/Main.java
@@ -1,12 +1,9 @@
 package com.zn0w.zfsr3d;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.ArrayList;
 
 import com.zn0w.zfsr3d.fs_components.FileTree;
-import com.zn0w.zfsr3d.fs_components.Node;
+import com.zn0w.zfsr3d.fs_components.FileTreeGenerator;
 
 public class Main {
 	
@@ -17,47 +14,10 @@ public class Main {
 		
         try {
         	@SuppressWarnings("unused")
-			FileTree fs = getFSTree(dirName);
+			FileTree fs = FileTreeGenerator.getFSTree(dirName);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	// TODO move methods below in a separate class
-	
-	static public FileTree getFSTree(String root_dir) throws IOException {
-		FileTree fs = new FileTree(new Node(root_dir));
-		
-		Files.list(new File(root_dir).toPath())
-        .forEach(path -> {
-            Node node = new Node(fs.getRoot(), path.getFileName().toString());
-            try {
-				getChildren(node, root_dir + "/");
-				fs.getRoot().addChild(node);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-        });
-		
-		return fs;
-	}
-	
-	static public void getChildren(Node node, String abs_path) throws IOException {
-		// if it's not a directory
-		if (node.getComponentName().contains(".")) {
-			return;
-		}
-		
-		Files.list(new File(abs_path + node.getComponentName()).toPath())
-        .forEach(path -> {
-            Node child_node = new Node(node, path.getFileName().toString());
-        	try {
-				getChildren(child_node, abs_path + node.getComponentName() + "/");
-				node.addChild(child_node);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-        });
 	}
 	
 }
---
+++ src/com/zn0w/zfsr3d/fs_components/FileTreeGenerator.java
@@ -0,0 +1,44 @@
+package com.zn0w.zfsr3d.fs_components;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class FileTreeGenerator {
+	
+	static public FileTree getFSTree(String root_dir) throws IOException {
+		FileTree fs = new FileTree(new Node(root_dir));
+		
+		Files.list(new File(root_dir).toPath())
+        .forEach(path -> {
+            Node node = new Node(fs.getRoot(), path.getFileName().toString());
+            try {
+				getChildren(node, root_dir + "/");
+				fs.getRoot().addChild(node);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+        });
+		
+		return fs;
+	}
+	
+	static public void getChildren(Node node, String abs_path) throws IOException {
+		// if it's not a directory
+		if (node.getComponentName().contains(".")) {
+			return;
+		}
+		
+		Files.list(new File(abs_path + node.getComponentName()).toPath())
+        .forEach(path -> {
+            Node child_node = new Node(node, path.getFileName().toString());
+        	try {
+				getChildren(child_node, abs_path + node.getComponentName() + "/");
+				node.addChild(child_node);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+        });
+	}
+	
+}
