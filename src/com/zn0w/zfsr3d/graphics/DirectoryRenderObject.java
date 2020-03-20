e7aee6d1bc5afefb5b44f20b2abda002be902075
Daniil Blagiy <blagiy.daniil@gmail.com>
2020-03-20 12:04:40 +0300

    RenderObject classes refactoring

--- src/com/zn0w/zfsr3d/graphics/DirectoryRenderObject.java
+++ src/com/zn0w/zfsr3d/graphics/DirectoryRenderObject.java
@@ -5,16 +5,8 @@ import java.awt.Graphics;
 
 public class DirectoryRenderObject extends RenderObject {
 	
-	public String name;
-	
-	
 	public DirectoryRenderObject(int x1, int y1, int x2, int y2, String name) {
-		this.x1 = x1;
-		this.y1 = y1;
-		this.x2 = x2;
-		this.y2 = y2;
-		
-		this.name = name;
+		super(x1, y1, x2, y2, name);
 	}
 	
 	@Override
--- src/com/zn0w/zfsr3d/graphics/RenderObject.java
+++ src/com/zn0w/zfsr3d/graphics/RenderObject.java
@@ -5,7 +5,17 @@ import java.awt.Graphics;
 public abstract class RenderObject {
 	
 	public int x1, y1, x2, y2;
+	public String name;
 	
+	
+	public RenderObject(int x1, int y1, int x2, int y2, String name) {
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+		
+		this.name = name;
+	}
 	
 	abstract void draw(Graphics g, int offset_x, int offset_y);
 	
