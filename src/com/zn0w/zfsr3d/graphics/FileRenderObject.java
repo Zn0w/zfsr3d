fb95aba3d8cf45e851e4d640e245184e40abdc13
Daniil Blagiy <blagiy.daniil@gmail.com>
2020-03-21 16:18:25 +0300

    RenderObject class and derived classes refactoring

--- src/com/zn0w/zfsr3d/graphics/FileRenderObject.java
+++ src/com/zn0w/zfsr3d/graphics/FileRenderObject.java
@@ -23,26 +23,7 @@ public class FileRenderObject extends RenderObject {
 		g.setColor(Color.blue);
 		g.fillOval(relative_x, relative_y, width, height);
 		
-		if (GlobalGraphicsSettings.SHOW_ALL_NAMES) {
-			g.setColor(Color.RED);
-			g.drawString(name, relative_x, relative_y);
-		}
-		
-		// TODO maybe simpler naming??
-		if (parent != null) {
-			int parent_relative_x = (int) ((parent.x1 + offset_x) * scale);
-			int parent_relative_y = (int) ((parent.y1 + offset_y) * scale);
-			int parent_width = (int) ((parent.x2 - parent.x1) * scale);
-			int parent_height = (int) ((parent.y2 - parent.y1) * scale);
-			
-			int child_relative_center_x = relative_x + width / 2;
-			int child_relative_center_y = relative_y + height / 2;
-			int parent_relative_center_x = parent_relative_x + parent_width / 2;
-			int parent_relative_center_y = parent_relative_y + parent_height / 2;
-			
-			g.setColor(Color.red);
-			g.drawLine(child_relative_center_x, child_relative_center_y, parent_relative_center_x, parent_relative_center_y);
-		}
+		common_conditional_draw(g, relative_x, relative_y, offset_x, offset_y, scale, width, height);
 	}
 	
 }
--- src/com/zn0w/zfsr3d/graphics/RenderObject.java
+++ src/com/zn0w/zfsr3d/graphics/RenderObject.java
@@ -1,5 +1,6 @@
 package com.zn0w.zfsr3d.graphics;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 public abstract class RenderObject {
@@ -7,6 +8,7 @@ public abstract class RenderObject {
 	public int x1, y1, x2, y2;
 	public String name;
 	public RenderObject parent;
+	public boolean hide_children;
 	
 	
 	public RenderObject(int x1, int y1, int x2, int y2, String name) {
@@ -27,6 +29,31 @@ public abstract class RenderObject {
 		
 		this.name = name;
 		this.parent = parent;
+	}
+	
+	protected void common_conditional_draw(
+			Graphics g, int relative_x, int relative_y, int offset_x, int offset_y, double scale, int width, int height
+			) {
+		if (GlobalGraphicsSettings.SHOW_ALL_NAMES) {
+			g.setColor(Color.RED);
+			g.drawString(name, relative_x, relative_y);
+		}
+		
+		// TODO maybe simpler naming??
+		if (parent != null) {
+			int parent_relative_x = (int) ((parent.x1 + offset_x) * scale);
+			int parent_relative_y = (int) ((parent.y1 + offset_y) * scale);
+			int parent_width = (int) ((parent.x2 - parent.x1) * scale);
+			int parent_height = (int) ((parent.y2 - parent.y1) * scale);
+			
+			int child_relative_center_x = relative_x + width / 2;
+			int child_relative_center_y = relative_y + height / 2;
+			int parent_relative_center_x = parent_relative_x + parent_width / 2;
+			int parent_relative_center_y = parent_relative_y + parent_height / 2;
+			
+			g.setColor(Color.red);
+			g.drawLine(child_relative_center_x, child_relative_center_y, parent_relative_center_x, parent_relative_center_y);
+		}
 	}
 	
 	abstract void draw(Graphics g, int offset_x, int offset_y, double scale);
